3c4bd1fd96af852c09b9b02e4a31b89ba5ee01a4
warabisan <amimotoryouta@gmail.com>
2025-08-14 16:52:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 const { Client } = require('discord.js');
 const client = new Client({ intents: [] }); // イベント監視だけ
 
-// ユーザートークンではなく、環境変数から読み込む形
-// process.env.USER_TOKEN に自分のユーザートークンを入れるとセルフボットになる（危険）
-const USER_TOKEN = process.env.USER_TOKEN;
-
 // 接続時イベント
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
